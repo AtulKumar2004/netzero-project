9943b2bad2ee44b259c8b28fd4981477f9d279d1
Atul Kumar <140424658+AtulKumar2004@users.noreply.github.com>
2025-07-12 10:36:21 +0530

    Fix Navigation Component Client Directive

--- components/ui/navigation.tsx
+++ components/ui/navigation.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { useState, useEffect } from 'react';
 import { Button } from '@/components/ui/button';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
